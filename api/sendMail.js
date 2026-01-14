d709354aae4e8ce4b10912065241b5bf47a733e3
owenprlce <158511754+owenprlce@users.noreply.github.com>
2026-01-14 15:42:23 -0800

    Fixk

--- api/sendMail.js
+++ api/sendMail.js
@@ -1,10 +1,10 @@
 import nodemailer from "nodemailer";
 
 export default async function handler(req, res) {
-
-  res.setHeader('Access-Control-Allow-Origin', 'https://owenprlce.github.io'); 
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');                
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', 'https://owenprlce.github.io');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
